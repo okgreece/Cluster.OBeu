1b448207af5edfb19d782fafc5e779601f78c167
kleanthisk10 <akis_k10@yahoo.com>
2016-08-30 17:23:37 +0300

    .

--- R/babbage.ts.obeu.R
+++ R/babbage.ts.obeu.R
@@ -35,7 +35,7 @@
 #' 
 #' @export
 ############################################################################
-babbage.ts.obeu<-function(json_data,time,amount,prediction_steps){
+babbage.ts.obeu2<-function(json_data,time,amount,prediction_steps){
   
   data <- jsonlite::fromJSON(json_data)
   data<-data$cells[-3]
@@ -43,21 +43,21 @@ babbage.ts.obeu<-function(json_data,time,amount,prediction_steps){
   data$time<-as.integer(data$time)
   
   # Check prediction_step
-  if( is.nan(prediction_steps)==T | is.na(prediction_steps)==T |
-      is.character(prediction_steps)==T | is.numeric(as.numeric(as.character(prediction_steps)))==F){
-    stop("Please give an integer input as 'prediction_steps', e.g. prediction_steps= 3.")}
+  #if( is.nan(prediction_steps)==T | is.na(prediction_steps)==T |
+   #   is.character(prediction_steps)==T | is.numeric(as.numeric(as.character(prediction_steps)))==F){
+    #stop("Please give an integer input as 'prediction_steps', e.g. prediction_steps= 3.")}
   
   # Check time
-  if( any(is.nan(data$time)==T) | any(is.na(data$time)==T) |
-      is.numeric(as.numeric(as.character(data$time)))==F | 
-      any(data$time>lubridate::year(now()))==T | any(data$time<1990)==T) {
-    stop("Please give a valid year input as 'time', time should be greater than 1990 and
-         could not be greater than the current year")}
+  #if( any(is.nan(data$time)==T) | any(is.na(data$time)==T) |
+   #   is.numeric(as.numeric(as.character(data$time)))==F | 
+    #  any(data$time>lubridate::year(now()))==T | any(data$time<1990)==T) {
+  #  stop("Please give a valid year input as 'time', time should be greater than 1990 and
+#         could not be greater than the current year")}
   
   # Check amount
-  if( any(is.nan(data$amount)==T) | any(is.na(data$amount)==T) |
-      is.character(data$amount)==T | is.numeric(as.numeric(as.character(data$amount)))==F){
-    stop("Please give a numeric input as 'amount'.")}
+  #if( any(is.nan(data$amount)==T) | any(is.na(data$amount)==T) |
+  #    is.character(data$amount)==T | is.numeric(as.numeric(as.character(data$amount)))==F){
+  #  stop("Please give a numeric input as 'amount'.")}
  
   tsdata<-ts(data$amount,start=min(data$time),end=max(data$time))
 
