85863097f473130b23d1f28f061e3bb7acc6acc5
Kleanthis Koupidis <koupidis.okfgr@gmail.com>
2017-10-31 16:37:50 +0200

    githubio site

---
+++ R/Sample city data.R
@@ -0,0 +1,18 @@
+#' Sample city data
+#' 
+#' @description 
+#' This dataset is an example data frame of the budget phase data
+#' 
+#' \itemize{
+#'   \item Administrative_Unit
+#'   \item Approved
+#'   \item Draft
+#'   \item Executed
+#'   \item Revised
+#'   }
+#' 
+#' @format A data frame with the previous characteristics as columns
+#' @name sample_city_data
+#' 
+#' 
+NULL
