584225ebea25b5bfd186fce2f1d1376e09ce15fd
Jaroslav Kuchar <jaroslav.kuchar@gmail.com>
2018-01-23 22:40:28 +0100

    cl.plot update

--- R/cl.plot.R
+++ R/cl.plot.R
@@ -10,10 +10,9 @@
 #' @seealso \code{\link{cl.analysis}}
 #' 
 #' @examples 
-#' #data("iris")
-#' #inputs.data <- scale(iris[,1:4])
-#' #inputs.clustering <- cl.analysis(inputs.data, cl.meth="kmeans", clust.numb=3)
-#' #cl.plot(inputs.clustering, parameters = list(convex.hulls=TRUE))
+#' library(Cluster.OBeu)
+#' inputs.clustering <- cl.analysis(sample_city_data, cl.meth="kmeans", clust.numb=3)
+#' cl.plot(inputs.clustering, parameters = list(ellipses=TRUE))
 #' 
 #' @import car
 #' @importFrom grDevices chull palette 
@@ -29,29 +28,33 @@ cl.plot <- function(clustering.model, parameters = list()) {
   if(is(clustering.model,"json")){
     clustering.model <- jsonlite::fromJSON(clustering.model)
   }
-  message(clustering.model$cl.meth)
+  # message(clustering.model$cluster.method)
   # kmeans
-  if(clustering.model$cl.meth %in% c("kmeans","pam","clara","funny")) {
+  if(clustering.model$cluster.method %in% c("kmeans","pam","clara","funny")) {
     # initialize
     parameters <- utils::modifyList(list(ellipses=FALSE, convex.hulls=FALSE), parameters)
     # PCA
-    inputs.pca <- stats::prcomp(clustering.model$data, scale. = T, center = T)
-    message("PCA summary:")
-    print(summary(inputs.pca))
+    #inputs.pca <- stats::prcomp(clustering.model$data, scale. = T, center = T)
+    #message("PCA summary:")
+    #print(summary(inputs.pca))
     par(oma=c(0, 0, 0, 5))
-    graphics::plot(inputs.pca$x[,1:2], main = clustering.model$cl.meth)
-    sapply(seq_along(unique(clustering.model$clusters)), function(clId) points(inputs.pca$x[which(clustering.model$clusters==unique(clustering.model$clusters)[clId]),1:2],col=palette()[clId]))
+    #graphics::plot(inputs.pca$x[,1:2], main = clustering.model$cl.meth)
+    graphics::plot(clustering.model$data.pca, main = clustering.model$cluster.method)
+    #sapply(seq_along(unique(clustering.model$clusters)), function(clId) points(inputs.pca$x[which(clustering.model$clusters==unique(clustering.model$clusters)[clId]),1:2],col=palette()[clId]))
+    sapply(seq_along(unique(clustering.model$clusters)), function(clId) points(clustering.model$data.pca[which(clustering.model$clusters==unique(clustering.model$clusters)[clId]),1:2],col=palette()[clId]))
     legend(par('usr')[2], par('usr')[4], bty='n', xpd=NA,
            unique(clustering.model$clusters), lwd = 1, lty = 1, pch = 1, col = palette()[unique(clustering.model$clusters)])
     # Ellipses
     if(parameters$ellipses == TRUE){
-      inputs.ellipses <- .ellipses(clustering.model, inputs.pca)
-      sapply(seq_along(inputs.ellipses), function(clId) lines(inputs.ellipses[[clId]], col=palette()[clId]))
+      #inputs.ellipses <- .ellipses(clustering.model, inputs.pca)
+      #sapply(seq_along(inputs.ellipses), function(clId) lines(inputs.ellipses[[clId]], col=palette()[clId]))
+      sapply(seq_along(clustering.model$cluster.ellipses), function(clId) lines(clustering.model$cluster.ellipses[[clId]], col=palette()[clId]))
     }
     # Convex hulls
     if(parameters$convex.hulls == TRUE) {
-      inputs.convexHulls <- .convex.hulls(clustering.model, inputs.pca)
-      sapply(seq_along(inputs.convexHulls), function(clId) lines(inputs.convexHulls[[clId]], col=palette()[clId]))
+      #inputs.convexHulls <- .convex.hulls(clustering.model, inputs.pca)
+      #sapply(seq_along(inputs.convexHulls), function(clId) lines(inputs.convexHulls[[clId]], col=palette()[clId]))
+      sapply(seq_along(clustering.model$cluster.convex.hulls), function(clId) lines(clustering.model$cluster.convex.hulls[[clId]], col=palette()[clId]))
     }
   } else {
     message(paste("Clusterring model", clustering.model$cluster.method, "is not supported!"))
